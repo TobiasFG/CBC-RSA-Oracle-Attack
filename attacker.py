883ea3d33bf830d8f975a46ea202919aa5df083c
TFG <tobias.fg@gmail.com>
2022-09-22 21:23:30 +0200

    asking oracle first step

--- attacker.py
+++ attacker.py
@@ -3,6 +3,7 @@ import requests
 
 # url of local server to attack
 URL = 'http://127.0.0.1:5000'
+URL_QUOTE = 'http://127.0.0.1:5000/quote'
 
 # get a cookie from the server with the authtoken
 
@@ -12,3 +13,15 @@ cookie = session.cookies.get_dict() # get the cookie
 
 authtoken = cookie['authtoken'] # The authtoken
 authtoken_bytes = bytes.fromhex(authtoken) # authtoken as bytes
+
+# check if padding is correct
+def check_cookie(authtoken):
+    cookie = {'authtoken': authtoken}
+    response = requests.get(URL_QUOTE, cookies=cookie)
+    if 'No quote for you!' in response.text:
+        return False
+    if '??' in response.text:
+        return True
+
+# see what happens when the received cookie is used directly
+print(check_cookie(authtoken))
