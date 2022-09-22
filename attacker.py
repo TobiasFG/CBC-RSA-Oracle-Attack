b25252caf012c6628927ecd6df6ea611b624e2b4
TFG <tobias.fg@gmail.com>
2022-09-22 21:13:39 +0200

    init attacker script

---
+++ attacker.py
@@ -0,0 +1,17 @@
+# Import request library
+import requests
+
+
+BLOCK_SIZE = 16 # Constant block size
+# url of local server to attack
+URL = 'http://127.0.0.1:5000'
+
+# get a cookie from the server with the authtoken
+
+session = requests.Session() # create a session
+session.get(URL) # get the url
+cookie = session.cookies.get_dict() # get the cookie
+
+authtoken = cookie['authtoken'] # The authtoken
+authtoken_bytes = bytes.fromhex(authtoken) # authtoken as bytes
+
