92b6b5e9a52980d2a112e519553b6e278def9dd4
Tobias Fabrin Gade <tobias.fg@gmail.com>
2022-09-23 15:05:24 +0200

    switch to mac

--- attacker.py
+++ attacker.py
@@ -1,5 +1,9 @@
 # Import request library
+from audioop import reverse
 import requests
+
+# Size of the IV block
+BLOCK_SIZE = 16
 
 # url of local server to attack
 URL = 'http://127.0.0.1:5000'
@@ -14,6 +18,15 @@ cookie = session.cookies.get_dict() # get the cookie
 authtoken = cookie['authtoken'] # The authtoken
 authtoken_bytes = bytes.fromhex(authtoken) # authtoken as bytes
 
+# Split the authtoken into blocks
+def split_blocks(bytes):
+    blocks = []
+    for i in range(0, len(bytes), BLOCK_SIZE):
+        blocks.append(bytes[i:i+BLOCK_SIZE])
+    return blocks
+
+authtoken_blocks = split_blocks(authtoken_bytes)
+
 # check if padding is correct
 def check_cookie(authtoken):
     cookie = {'authtoken': authtoken}
@@ -26,13 +39,22 @@ def check_cookie(authtoken):
 # see what happens when the received cookie is used directly
 print(check_cookie(authtoken))
 
-# IV 
-IV = []
+IV = [] # Initialization Vector
+PAD = [] # Padding Vector
 
-for auth_byte_index in range(len(authtoken_bytes)):
+# For each byte in the block last block
+block = authtoken_blocks[-1]
+
+for byte in range(len(block)):
     for tester in range(256):
-        # if authtoken_bytes[auth_byte_index] xor tester == auth_byte then we have the correct byte
-        if (authtoken_bytes[auth_byte_index] ^ tester == auth_byte_index):
-            # prepend the byte to the IV
-            IV.insert(0, tester)
+        # if block[byte] xor tester == byte then we have the correct byte
+        if (block[byte] ^ tester == byte):
+            # append the byte to the IV
+            IV.append(tester)
             break
+    # Create a Zero Initialization Vector
+    ZIV = [byte] * len(IV)
+    # take the auth_byte_index first bytes of the auth_token
+    xorer = block[:byte]
+    # xor the ZIV with the xorer
+    IV = xorer ^ ZIV
